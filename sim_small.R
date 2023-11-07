99a6df8eccf591325fb66582f1cc94510049778a
Noah Kochanski <noahkoch@umich.edu>
2023-11-07 11:44:08 -0500

    adding test simulation

---
+++ sim_small.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+library(mclust)
+library(teigen)
+library(doParallel)
+library(transport)
+source('sim_function.R')
+
+cl <- makeCluster(32)
+registerDoParallel(cl)
+
+run_sim(alpha = 0.05, 
+        n = 500,
+        num_of_sim = 2,
+        eq_var = T, 
+        scale = c(1,1),
+        diffs = seq(0.2, 4, by = 0.1))
