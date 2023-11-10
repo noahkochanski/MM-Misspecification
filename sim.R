ec0abb953132f39e3a78c8bd0cdc51bb194c3d18
Noah Kochanski <noahkoch@umich.edu>
2023-11-09 19:55:37 -0500

    testing sim

--- sim.R
+++ sim.R
@@ -5,33 +5,10 @@ library(doParallel)
 library(transport)
 source('sim_function.R')
 
-cl <- makeCluster(32)
+cl <- makeCluster(2)
 registerDoParallel(cl)
 
-B = 1000
-
-# equal scale
-run_sim(alpha = 0.05, 
-  n = 500,
-  num_of_sim = B,
-  eq_var = T, 
-  scale = c(1,1),
-  diffs = seq(0.2, 5, by = 0.1))
-
-run_sim(alpha = 0.05, 
-        n = 1000,
-        num_of_sim = B,
-        eq_var = T, 
-        scale = c(1,1),
-        diffs = seq(0.2, 5, by = 0.1))
-
-
-run_sim(alpha = 0.05, 
-        n = 2000,
-        num_of_sim = B,
-        eq_var = T, 
-        scale = c(1,1),
-        diffs = seq(0.2, 5, by = 0.1))
+B = 1
 
 # 2-1 scale
 
